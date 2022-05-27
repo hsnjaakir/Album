29edbce573e40e09388c46c8371228fc7ecb3704
Jakir Hosen <61291355+hsnjaakir@users.noreply.github.com>
2022-05-27 15:26:34 +0600

    Update Posts.js

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -3,7 +3,7 @@ import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
 import Post from './Post/Post';
-import useStyles from '../../styles';
+import useStyles from './style';
 
 const Posts = ({ setCurrentId }) => {
   const posts = useSelector((state) => state.posts);
