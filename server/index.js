e4ee0e2aeeee2a417ee7c9d0abe78239c341b593
hsnjaakir <jakir0613@gmail.com>
2022-06-05 20:33:52 +0600

    first change

--- server/index.js
+++ server/index.js
@@ -2,23 +2,30 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.get('/', (req, res) => {
+  res.send('Hello to album API');
+});
 
+const PORT = process.env.PORT || 5000;
 const CONNECTION_URL =
   'mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.b1mpy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
